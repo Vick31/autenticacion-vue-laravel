e0824660a7677c1fb88dbabb7a867d9848aaa816
Robinson Rivera Villamizar <qstecno@gmail.com>
2022-08-19 17:03:45 -0500

    nav2

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <template>
-<nav class="navbar bg-light">
+<nav class="navbar bg-light" id="barra">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">
-      <img src="/docs/5.2/assets/brand/bootstrap-logo.svg" alt="" width="30" height="24" class="d-inline-block align-text-top">
-      Bootstrap
-    </a>
-  </div>
-
-  <router-link class="link" to="/login">Acceder</router-link>
-  <br><br>
+     <img src="/img/logo.png" alt="logo" width="100" height="100" class="d-inline-block align-text-top">
+      
+      <h2>ComidApp</h2>
+    
+  
+<div class="icons_login">
+  <router-link class="link" to="/login">Acceder</router-link>  
   <router-link class="link" to="/register">Registrarse</router-link>
-  <br><br>
+  </div>
+  
   <router-link v-if="this.$route.path == '/account'" class="link" to="/account">Tu cuenta</router-link>
+  </div>
   </nav>
   <br><br>
   <div class="container">
@@ -19,5 +20,9 @@
     </router-view>
   </div>
   
+  
 
 </template>
+<style scoped>
+    @import "../src/assets/css/styleApp.css";
+  </style>
