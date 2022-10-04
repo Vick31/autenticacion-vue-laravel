0ecfa9dd34db753235c51933eb741637e01a960c
Manuel Stiven Pérez Victoria <89532383+Vick31@users.noreply.github.com>
2022-10-04 17:32:29 -0500

    actualizacion

--- src/components/Select.vue
+++ src/components/Select.vue
@@ -8,25 +8,40 @@
                     src="https://www.google.com/maps/embed?pb=!1m23!1m12!1m3!1d2303.8923872806686!2d-75.59563870111862!3d1.6288451132891066!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m8!3e6!4m0!4m5!1s0x8e2451f51faf07c5%3A0x3e70f4efd3e1d405!2sRANCHO%20BURGER%2C%20Cl%2016%2C%20Florencia%2C%20Caquet%C3%A1!3m2!1d1.6289311!2d-75.59517439999999!5e0!3m2!1ses!2sco!4v1661783624907!5m2!1ses!2sco"
                     allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
             </div>
+
+            <div class="description-company">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor voluptate exercitationem incidunt
+                laudantium minima! Porro illum, obcaecati officiis iusto voluptatibus necessitatibus quidem expedita
+                animi deserunt eligendi hic at culpa. Odit?
+            </div>
+
             <div class="company">
                 <h4> {{ company.name }} </h4>
-                <h4>logo</h4>
+                <img :src="company.logo" alt="">
             </div>
         </div>
-        <div>
-            <br>
+
+        <div class="container-products">
+
             <h2>Elige tu comida</h2>
-            <div>
-                <router-link to="/select/topings">
-                    <div class="empresa button" v-for="p in list_hambuguer" @click="insertar(p.name)">
-                        <div class="card">
-                            <img src="../../img/f.elconfidencial.com_original_cd5_15e_c44_cd515ec44327e3f273fd91e12098d635.jpg"
-                                alt="">
+
+            <div class="products">
+
+                <div class="empresa button" v-for="p in list_hambuguer" @click="insertar(p.name)">
+
+                    <router-link class="links" to="/select/topings">
+
+                        <div class="div-img">
+                            <img src="../../img/comidas/img1.jpg" alt="">
+                        </div>
+
+                        <div class="content">
                             <h4>{{ p.name }}</h4>
                             <h4>${{ p.price }} COP</h4>
                         </div>
-                    </div>
-                </router-link>
+
+                    </router-link>
+                </div>
             </div>
         </div>
     </div>
